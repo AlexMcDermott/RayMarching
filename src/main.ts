ec6a6d20b62fe1460e14f0218b87862b7ab0df5d
Alex McDermott <alexjmcd2202@gmail.com>
2019-11-07 16:28:58 +1000

    Revert "Added camera object"
    This reverts commit d798a32cd02989d9a468cdb0e83535bbd7bc3ba3.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import * as dat from 'dat.gui';
 import { mat4, vec2, vec3 } from 'gl-matrix';
 import * as twgl from 'twgl.js';
-import Camera from './camera';
 
 // @ts-ignore
 import fragmentSource from './shader/fragment.glsl';
@@ -21,7 +20,7 @@ const arrays = {
 };
 const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
 
-let uniforms = {
+const uniforms = {
   maxSteps: 200,
   minDist: 0,
   maxDist: 50,
@@ -44,10 +43,69 @@ let uniforms = {
   specularPower: 10,
 };
 
-const cam = new Camera(cnv, uniforms);
+const rotation = vec2.create();
+const xRotMatrix = mat4.create();
+const yRotMatrix = mat4.create();
+const d2r = Math.PI / 180;
+const keyStates = {
+  w: false,
+  a: false,
+  s: false,
+  d: false,
+};
+
+function updateKeyStates(e: KeyboardEvent) {
+  if (!e.repeat) {
+    for (const key of Object.keys(keyStates)) {
+      if (e.key === key) {
+        keyStates[key] = !keyStates[key];
+        break;
+      }
+    }
+  }
+}
+
+document.addEventListener('keydown', updateKeyStates);
+document.addEventListener('keyup', updateKeyStates);
+
+function update() {
+  mat4.fromXRotation(xRotMatrix, rotation[0]);
+  mat4.fromYRotation(yRotMatrix, rotation[1]);
+  mat4.multiply(uniforms.rotationMatrix, yRotMatrix, xRotMatrix);
+
+  if (document.pointerLockElement === cnv) {
+    const forward = vec3.fromValues(0, 0, -1);
+    const right = vec3.create();
+    const up = vec3.create();
+    vec3.transformMat4(forward, forward, uniforms.rotationMatrix);
+    vec3.cross(right, forward, [0, 1, 0]);
+    vec3.cross(up, right, forward);
+    vec3.normalize(forward, forward);
+    vec3.normalize(right, right);
+    vec3.normalize(up, up);
+    vec3.scale(forward, forward, uniforms.movementSpeed);
+    vec3.scale(right, right, uniforms.movementSpeed);
+    vec3.scale(up, up, uniforms.movementSpeed);
+
+    if (keyStates.w) {
+      vec3.add(uniforms.cameraPos, uniforms.cameraPos, forward);
+    }
+    if (keyStates.a) {
+      vec3.subtract(uniforms.cameraPos, uniforms.cameraPos, right);
+    }
+    if (keyStates.s) {
+      vec3.subtract(uniforms.cameraPos, uniforms.cameraPos, forward);
+    }
+    if (keyStates.d) {
+      vec3.add(uniforms.cameraPos, uniforms.cameraPos, right);
+    }
+
+    vec3.multiply(uniforms.cameraPos, uniforms.cameraPos, [1, 0, 1]);
+  }
+}
 
 function render() {
-  uniforms = cam.update();
+  update();
   twgl.resizeCanvasToDisplaySize(cnv);
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   gl.useProgram(programInfo.program);
@@ -56,6 +114,24 @@ function render() {
   twgl.drawBufferInfo(gl, bufferInfo);
   requestAnimationFrame(render);
 }
+
+cnv.addEventListener('click', () => {
+  cnv.requestPointerLock();
+});
+
+function clamp(val: number, min: number, max: number) {
+  return Math.min(Math.max(min, val), max);
+}
+
+document.addEventListener('mousemove', e => {
+  if (document.pointerLockElement === cnv) {
+    const movement = vec2.fromValues(-e.movementY, -e.movementX);
+    vec2.scale(movement, movement, uniforms.mouseSens * uniforms.FOV * d2r);
+    vec2.add(rotation, rotation, movement);
+    const range = uniforms.xRotMax * d2r;
+    rotation[0] = clamp(rotation[0], -range, range);
+  }
+});
 
 window.addEventListener('resize', () => {
   cnv.width = window.innerWidth;
