fd082824ad8e2183c009aadc9f518d3ac321545b
Alex McDermott <alexjmcd2202@gmail.com>
2019-11-11 10:51:55 +1100

    Split out handler functions from listeners

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,9 @@ const uniforms = {
   xRotMax: 90,
   mouseSens: 0.0015,
   movementSpeed: 0.05,
+  keyStates: { w: false, a: false, s: false, d: false },
   cameraPos: vec3.fromValues(0, 0, 0),
+  rotation: vec2.create(),
   rotationMatrix: mat4.create(),
   lightPos: vec3.fromValues(5, 5, 5),
   objectPos: vec3.fromValues(0, 0, -5),
@@ -43,69 +45,82 @@ const uniforms = {
   specularPower: 10,
 };
 
-const rotation = vec2.create();
-const xRotMatrix = mat4.create();
-const yRotMatrix = mat4.create();
-const d2r = Math.PI / 180;
-const keyStates = {
-  w: false,
-  a: false,
-  s: false,
-  d: false,
-};
+function updateRotation() {
+  const xRotMatrix = mat4.create();
+  const yRotMatrix = mat4.create();
+  mat4.fromXRotation(xRotMatrix, uniforms.rotation[0]);
+  mat4.fromYRotation(yRotMatrix, uniforms.rotation[1]);
+  mat4.multiply(uniforms.rotationMatrix, yRotMatrix, xRotMatrix);
+}
 
-function updateKeyStates(e: KeyboardEvent) {
+function updatePosition() {
+  const forward = vec3.fromValues(0, 0, -1);
+  const right = vec3.create();
+  const up = vec3.create();
+  vec3.transformMat4(forward, forward, uniforms.rotationMatrix);
+  vec3.cross(right, forward, [0, 1, 0]);
+  vec3.cross(up, right, forward);
+  vec3.normalize(forward, forward);
+  vec3.normalize(right, right);
+  vec3.scale(forward, forward, uniforms.movementSpeed);
+  vec3.scale(right, right, uniforms.movementSpeed);
+
+  if (uniforms.keyStates.w) {
+    vec3.add(uniforms.cameraPos, uniforms.cameraPos, forward);
+  }
+  if (uniforms.keyStates.a) {
+    vec3.subtract(uniforms.cameraPos, uniforms.cameraPos, right);
+  }
+  if (uniforms.keyStates.s) {
+    vec3.subtract(uniforms.cameraPos, uniforms.cameraPos, forward);
+  }
+  if (uniforms.keyStates.d) {
+    vec3.add(uniforms.cameraPos, uniforms.cameraPos, right);
+  }
+
+  vec3.multiply(uniforms.cameraPos, uniforms.cameraPos, [1, 0, 1]);
+}
+
+function handleKey(e: KeyboardEvent) {
   if (!e.repeat) {
-    for (const key of Object.keys(keyStates)) {
+    for (const key of Object.keys(uniforms.keyStates)) {
       if (e.key === key) {
-        keyStates[key] = !keyStates[key];
+        uniforms.keyStates[key] = !uniforms.keyStates[key];
         break;
       }
     }
   }
 }
 
-document.addEventListener('keydown', updateKeyStates);
-document.addEventListener('keyup', updateKeyStates);
+function handleClick() {
+  cnv.requestPointerLock();
+}
 
-function update() {
-  mat4.fromXRotation(xRotMatrix, rotation[0]);
-  mat4.fromYRotation(yRotMatrix, rotation[1]);
-  mat4.multiply(uniforms.rotationMatrix, yRotMatrix, xRotMatrix);
-
+function handleMouseMove(e: MouseEvent) {
   if (document.pointerLockElement === cnv) {
-    const forward = vec3.fromValues(0, 0, -1);
-    const right = vec3.create();
-    const up = vec3.create();
-    vec3.transformMat4(forward, forward, uniforms.rotationMatrix);
-    vec3.cross(right, forward, [0, 1, 0]);
-    vec3.cross(up, right, forward);
-    vec3.normalize(forward, forward);
-    vec3.normalize(right, right);
-    vec3.normalize(up, up);
-    vec3.scale(forward, forward, uniforms.movementSpeed);
-    vec3.scale(right, right, uniforms.movementSpeed);
-    vec3.scale(up, up, uniforms.movementSpeed);
-
-    if (keyStates.w) {
-      vec3.add(uniforms.cameraPos, uniforms.cameraPos, forward);
-    }
-    if (keyStates.a) {
-      vec3.subtract(uniforms.cameraPos, uniforms.cameraPos, right);
-    }
-    if (keyStates.s) {
-      vec3.subtract(uniforms.cameraPos, uniforms.cameraPos, forward);
-    }
-    if (keyStates.d) {
-      vec3.add(uniforms.cameraPos, uniforms.cameraPos, right);
-    }
-
-    vec3.multiply(uniforms.cameraPos, uniforms.cameraPos, [1, 0, 1]);
+    const movement = vec2.fromValues(-e.movementY, -e.movementX);
+    const factor = uniforms.mouseSens * uniforms.FOV * (Math.PI / 180);
+    vec2.scale(movement, movement, factor);
+    vec2.add(uniforms.rotation, uniforms.rotation, movement);
+    const range = uniforms.xRotMax * (Math.PI / 180);
+    uniforms.rotation[0] = Math.min(
+      Math.max(-range, uniforms.rotation[0]),
+      range
+    );
   }
 }
 
+function handleResize() {
+  cnv.width = window.innerWidth;
+  cnv.height = window.innerHeight;
+  uniforms.resolution = vec2.fromValues(cnv.width, cnv.height);
+}
+
 function render() {
-  update();
+  if (document.pointerLockElement === cnv) {
+    updateRotation();
+    updatePosition();
+  }
   twgl.resizeCanvasToDisplaySize(cnv);
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   gl.useProgram(programInfo.program);
@@ -115,42 +130,22 @@ function render() {
   requestAnimationFrame(render);
 }
 
-cnv.addEventListener('click', () => {
-  cnv.requestPointerLock();
-});
+const gui = new dat.GUI();
+gui.add(uniforms, 'FOV', 1, 179);
+gui.add(uniforms, 'xRotMax', 1, 90);
+gui.add(uniforms, 'mouseSens', 0, 0.005);
+gui.add(uniforms, 'movementSpeed', 0, 0.5);
+gui.addColor(uniforms, 'objectColour');
+gui.addColor(uniforms, 'worldColour');
+gui.add(uniforms, 'worldColourFactor', 0, 1);
+gui.add(uniforms, 'diffuseFactor', 0, 1);
+gui.add(uniforms, 'specularFactor', 0, 1);
+gui.add(uniforms, 'ambientMin', 0, 1);
+gui.add(uniforms, 'specularPower', 1, 50);
+requestAnimationFrame(render);
 
-function clamp(val: number, min: number, max: number) {
-  return Math.min(Math.max(min, val), max);
-}
-
-document.addEventListener('mousemove', e => {
-  if (document.pointerLockElement === cnv) {
-    const movement = vec2.fromValues(-e.movementY, -e.movementX);
-    vec2.scale(movement, movement, uniforms.mouseSens * uniforms.FOV * d2r);
-    vec2.add(rotation, rotation, movement);
-    const range = uniforms.xRotMax * d2r;
-    rotation[0] = clamp(rotation[0], -range, range);
-  }
-});
-
-window.addEventListener('resize', () => {
-  cnv.width = window.innerWidth;
-  cnv.height = window.innerHeight;
-  uniforms.resolution = vec2.fromValues(cnv.width, cnv.height);
-});
-
-window.addEventListener('load', () => {
-  const gui = new dat.GUI();
-  gui.add(uniforms, 'FOV', 1, 179);
-  gui.add(uniforms, 'xRotMax', 1, 90);
-  gui.add(uniforms, 'mouseSens', 0, 0.005);
-  gui.add(uniforms, 'movementSpeed', 0, 0.5);
-  gui.addColor(uniforms, 'objectColour');
-  gui.addColor(uniforms, 'worldColour');
-  gui.add(uniforms, 'worldColourFactor', 0, 1);
-  gui.add(uniforms, 'diffuseFactor', 0, 1);
-  gui.add(uniforms, 'specularFactor', 0, 1);
-  gui.add(uniforms, 'ambientMin', 0, 1);
-  gui.add(uniforms, 'specularPower', 1, 50);
-  requestAnimationFrame(render);
-});
+document.addEventListener('keydown', handleKey);
+document.addEventListener('keyup', handleKey);
+window.addEventListener('resize', handleResize);
+document.addEventListener('mousemove', handleMouseMove);
+cnv.addEventListener('click', handleClick);
